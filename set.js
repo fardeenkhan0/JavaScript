307e60d467b2a720dc38919a00c201698130bc11
fardeenkhan0 <fardeenkhan1041@gamil.com>
2023-10-25 17:37:40 +0530

    25-oct

---
+++ set.js
@@ -0,0 +1,5 @@
+//document.write("hello")
+document.getElementById("a").innerHTML = "<center><h2>PNINFOSY GWALIOR 1234</h>"
+document.getElementById("a").setAttribute("style","border: 10px solid red");
+//document.getElementById("a").removeAttribute("style");
+document.getElementById("b").innerHTML = "<center><h2>MR FARDEEN KHAN</h>"
