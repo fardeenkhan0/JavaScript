8f41c628ab87846533e5405e6f72ebf5a07016dc
fardeenkhan0 <fardeenkhan1041@gamil.com>
2023-10-27 17:45:41 +0530

    27-10oct

--- to-do.js
+++ to-do.js
@@ -1,7 +1,22 @@
 const addUserBtn = document.getElementById('addUser')
 const userNameTextField = document.getElementById('username')
 
-addUserBtn.onclick = () =>{
+let userArray = []; //data store
+addUserBtn.onclick = () => {
     //alert("hello")
-    const name = userNameTextField.Value;
+    const name = userNameTextField.value;
+    //alert(name)
+    userArray.push({ 'name': name });
+    //console.log(userArray);
+    savedata(userArray)
+
+    function savedata(userArray)
+    {
+        //console.log(userArray)
+        let str = JSON.stringify(userArray);//convert object to string
+         //console.log(str)
+         localStorage.setItem('users',str);
+
+    }
+
 }
