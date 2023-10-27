855942fe84f8bb1c1f6657571ad0579e7e132c9b
fardeenkhan0 <fardeenkhan1041@gamil.com>
2023-10-27 17:08:31 +0530

    28

---
+++ to-do.js
@@ -0,0 +1,7 @@
+const addUserBtn = document.getElementById('addUser')
+const userNameTextField = document.getElementById('username')
+
+addUserBtn.onclick = () =>{
+    //alert("hello")
+    const name = userNameTextField.Value;
+}
